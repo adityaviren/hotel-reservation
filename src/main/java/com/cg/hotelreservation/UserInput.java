f691a487b78d72a682193a3ab6e02ae984114071
Aditya Viren <adityaviren@gmail.com>
2020-11-11 18:22:56 +0530

    REFACTOR to create test cases and ifferent methods

--- src/main/java/com/cg/hotelreservation/UserInput.java
+++ src/main/java/com/cg/hotelreservation/UserInput.java
@@ -12,54 +12,51 @@ import static java.time.temporal.ChronoUnit.DAYS;
 public class UserInput {
 
     Scanner scanner = new Scanner(System.in);
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("ddMMMyyyy");
 
     public static void main(String[] args) {
         System.out.println("WELCOME TO HOTEL RESERVATION PROGRAM");
         UserInput userInput = new UserInput();
-        userInput.uc1();
+        Date[] dateArray=new Date[2];
+        dateArray = userInput.input();
+        userInput.uc1(dateArray[0],dateArray[1]);
+    }
+
+    public Date[] input(){
+
+        Date start,end;
+        while(true) {
+            System.out.println("Enter the start date in the format ddMMMYYYY");
+            start = returnStartDate(scanner.nextLine());
+            if(start!=null)
+                break;
+        }
+        while (true){
+            System.out.println("Enter the end date in the format ddMMMYYYY");
+            end = returnEndDate(scanner.nextLine());
+            if(end!=null)
+                break;
+        }
+        Date[] dateArray=new Date[2];
+        dateArray[0]=start;
+        dateArray[1]=end;
+        return dateArray;
     }
 
     //UC1 basic rates
-    public void uc1(){
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("ddMMMyyyy");
-        Date start,end;
-        LocalDate startF,endF;
-        while(true) {
-            System.out.println("Enter the start date in the format ddMMMYYYY");
-            try {
-                start = simpleDateFormat.parse(scanner.nextLine());
-                startF = convertToLocalDateViaSqlDate(start);
-                break;
-            } catch (ParseException e) {
-                System.out.println("Enter proper date");;
-            }
-        }
-        while (true){
-                System.out.println("Enter the end date in the format ddMMMYYYY");
-            try {
-                end = simpleDateFormat.parse(scanner.nextLine());
-                endF = convertToLocalDateViaSqlDate(end);
-                System.out.println(endF);
-                endF=endF.plusDays(1);
-                System.out.println(endF);
-                end=convertToDateViaSqlDate(endF);
-                break;
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        }
+    public int uc1(Date start, Date end){
         long difference = end.getTime()-start.getTime();
         int numOfDays = (int) Math.floor(difference/(3600*24*1000));
         int bridgewood_rates=bridgewoodRates(numOfDays);
         int lakewood_rates=lakewoodRates(numOfDays);
         int ridgewood_rates=ridgewoodRates(numOfDays);
         if(lakewood_rates<bridgewood_rates&&lakewood_rates<ridgewood_rates){
-            System.out.println("LakeWood , Total Rates: " + lakewood_rates);
+            return lakewood_rates;
         }
         else if(bridgewood_rates<lakewood_rates&&bridgewood_rates<ridgewood_rates)
-            System.out.println("BridgeWood , Total Rates: " + bridgewood_rates);
+            return bridgewood_rates;
         else
-            System.out.println("RidgeWood , Total Rates: " + ridgewood_rates);
+            return ridgewood_rates;
     }
 
     public int lakewoodRates(int numOfDays){
@@ -81,5 +78,25 @@ public class UserInput {
 
     public Date convertToDateViaSqlDate(LocalDate dateToConvert) {
         return java.sql.Date.valueOf(dateToConvert);
+    }
+
+    public Date returnStartDate(String start){
+        try {
+            return simpleDateFormat.parse(start);
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+
+    public Date returnEndDate(String end){
+        try {
+            Date date = simpleDateFormat.parse(end);
+            LocalDate localDate = convertToLocalDateViaSqlDate(date);
+            localDate=localDate.plusDays(1);
+            date=convertToDateViaSqlDate(localDate);
+            return date;
+        } catch (ParseException e) {
+            return null;
+        }
     }
 }
--- src/test/java/com/cg/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/cg/hotelreservation/HotelReservationTest.java
@@ -1,5 +1,6 @@
 package com.cg.hotelreservation;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.text.ParseException;
@@ -9,10 +10,11 @@ import java.util.Date;
 public class HotelReservationTest {
 
     @Test
-    public void inputDate() throws ParseException {
-        String date="13sep2014";
-        SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMMyyyy");
-        Date date1=dateFormat.parse(date);
-        System.out.println(date1);
+    public void givenDate_shouldReturnLowestRate_whenPassedThroughInput() {
+        UserInput userInput = new UserInput();
+        Date start = userInput.returnStartDate("12sep2020");
+        Date end = userInput.returnEndDate("13sep2020");
+        int minRate = userInput.uc1(start,end);
+        Assert.assertEquals(220,minRate);
     }
 }
